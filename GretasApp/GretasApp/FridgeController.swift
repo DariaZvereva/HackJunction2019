6630b5c75826b2fc5adb1582b24ee8de6c42ca57
DariaZvereva <zverevads@gmail.com>
2019-11-16 18:22:54 +0200

    Add button and rectangles

--- GretasApp/GretasApp/FridgeController.swift
+++ GretasApp/GretasApp/FridgeController.swift
@@ -8,22 +8,50 @@
 
 import UIKit
 
+
 class FridgeController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
     @IBOutlet weak var pageLabel: UILabel!
     
     @IBOutlet weak var productsStackView: UIStackView!
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.scrollView.addSubview(self.productsStackView)
+    self.productsStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+          // Attaching the content's edges to the scroll view's edges
+          productsStackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+          productsStackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+          productsStackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+          productsStackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+
+          // Satisfying size constraints
+          productsStackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+        ])
+    }
+    
     @IBAction func addProduct(_ sender: Any) {
-        let view1 = UIView(frame: CGRect(x: 100, y: 50, width: 100, height: 100))
-        view1.backgroundColor = UIColor.red
-        productsStackView.addArrangedSubview(view1)
+//        let view1 = UIView(frame: CGRect(x: 100, y: 50, width: 100, height: 100))
+//        view1.backgroundColor = UIColor.red
+//        productsStackView.addArrangedSubview(view1)
+        let greenView = UIView()
+        greenView.backgroundColor = .green
+        productsStackView.addArrangedSubview(greenView)
+        greenView.translatesAutoresizingMaskIntoConstraints = false
+        // Doesn't have intrinsic content size, so we have to provide the height at least
+        greenView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+
+        // Label (has instrinsic content size)
+        let label = UILabel()
+        label.backgroundColor = .orange
+        label.text = "I'm label \(self.productsStackView.subviews.count)."
+        label.textAlignment = .center
+        productsStackView.addArrangedSubview(label)
     }
     /*
     // MARK: - Navigation
